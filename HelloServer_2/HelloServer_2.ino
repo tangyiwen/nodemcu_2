8d49789b622719b5106d9ab13f64558c097fde41
tangyiwen <fa33053@gmail.com>
2016-11-30 17:11:05 +0800

    Add files via upload

---
+++ HelloServer_2/HelloServer_2.ino
@@ -0,0 +1,102 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <Wire.h>
+
+const char* ssid = "Pluto";
+const char* password = "pluto27351";
+
+ESP8266WebServer server(80);
+
+const int led = 13;
+char led_status[25];
+String webSite;
+byte x = 0;
+void buildWeb() {
+  webSite += "<html>";
+  webSite+="<head>\n";
+  webSite+="<meta charset='UTF-8'/>\n";
+  webSite+="<title>Form Control RGB_LED</title>\n";
+  webSite+="<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js\"></script>";
+  webSite+="<script>";
+  webSite+="$(document).ready(function(){";
+  webSite+="$(\"[type = range]\").change(function(){";
+  webSite+="var redval = $(\"#red\").val();";
+  webSite+="$(\"#red-text\").text(redval);";
+   webSite+="var greenval = $(\"#green\").val();";
+  webSite+="$(\"#green-text\").text(greenval);";
+   webSite+="var blueval = $(\"#blue\").val();";
+  webSite+="$(\"#blue-text\").text(blueval);";
+  webSite+="});";
+  webSite+="});";
+  webSite+="</script>";
+  webSite+="</head>\n";
+  webSite += "<body>";
+  webSite += "<h1>RGB LED Control form</h1>";
+  webSite += "<form action=\"/form1\">";
+  webSite += "<p>Red Color : <span id = \"red-text\">0</span></p>";
+  webSite += "<input type=\"range\" id=\"red\" name=\"R-led\" min=\"0\" max=\"255\" value=\"0\">";
+  webSite += "<p>Green Color : <span id = \"green-text\">0</span></p>";
+  webSite += "<input type=\"range\" id=\"green\" name=\"G-led\" min=\"0\" max=\"255\" value=\"0\">";
+  webSite += "<p>Blue Color : <span id = \"blue-text\">0</span></p>";
+  webSite += "<input type=\"range\" id=\"blue\" name=\"B-led\" min=\"0\" max=\"255\" value=\"0\" ><br><br>";
+  webSite += "<input type=\"submit\" value=\"submit\"></form>";
+  webSite += "</body></html>";
+   
+}
+
+void handleRoot() {
+  server.send ( 200, "text/html", webSite ); //send back client a HTML file
+}
+
+void handleLEDStatus() {
+  int red_color = server.arg("R-led").toInt();
+  int green_color = server.arg("G-led").toInt();
+  int blue_color = server.arg("B-led").toInt();
+  Wire.beginTransmission(8); // transmit to device #8
+      // sends five bytes
+  Wire.write(red_color);              // sends one byte
+        // sends five bytes
+  Wire.write(green_color);              // sends one byte
+    // sends five bytes
+  Wire.write(blue_color);              // sends one byte
+  
+  Wire.endTransmission();    // stop transmitting
+}
+
+
+void handleNotFound() {
+  server.send ( 404, "text/html", "404 Error!" );  
+}
+void setup(void){
+  buildWeb();
+  pinMode(led, OUTPUT);
+  digitalWrite(led, 0);
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  server.on ( "/", handleRoot );
+  server.on("/form1",handleLEDStatus);
+  server.onNotFound ( handleNotFound );
+  Wire.begin(4,5);
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop(void){
+  server.handleClient();
+}
---
+++ sketch_nov30b_2/sketch_nov30b_2.ino
@@ -0,0 +1,52 @@
+//uno
+
+#include <Wire.h>
+
+#define LEDR 3
+#define LEDG 5
+#define LEDB 6
+
+
+void setup() {
+  Wire.begin(8);                // join i2c bus with address #8
+  Wire.onReceive(receiveEvent); // register event
+  Serial.begin(115200);           // start serial for output
+}
+
+void turnoff(){
+  digitalWrite(LEDR, LOW);
+  digitalWrite(LEDG, LOW);
+  digitalWrite(LEDB, LOW);
+}
+
+
+void loop() {
+  delay(100);
+}
+
+// function that executes whenever data is received from master
+// this function is registered as an event, see setup()
+void receiveEvent(int howMany) {
+  
+/*char x = Wire.read();    // receive byte as an integer
+  Serial.println(x);         // print the integer
+  int y = Wire.read();    // receive byte as an integer
+  Serial.println(y);         // print the integer
+  int z = Wire.read();    // receive byte as an integer
+  Serial.println(z);         // print the integer*/
+ 
+   int r = Wire.read(); // receive byte as a character
+   Serial.print(r);         // print the character
+     int g= Wire.read(); // receive byte as a character
+   Serial.print(g);         // print the character
+     int b = Wire.read(); // receive byte as a character
+   Serial.print(b);         // print the character
+
+   analogWrite(LEDR,r);
+  analogWrite(LEDG,g);
+  analogWrite(LEDB,b);
+  
+  
+  
+}
+
